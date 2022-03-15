575d81cdca0d89d02a44d67a33964dfcbc22e3e0
Sonja Rouwhorst <s.e.rouwhorst@hva.nl>
2022-03-15 21:08:46 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,24 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendValue("aanval", 30)
+})
+radio.onReceivedValue(function (name, value) {
+    if (hp > 0) {
+        hp = hp - value
+    }
+})
+let hp = 0
+hp = 90
+radio.setGroup(33)
+radio.setTransmitPower(1)
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+    if (hp > 70) {
+        basic.showIcon(IconNames.Happy)
+    } else if (hp > 40 && hp <= 70) {
+        basic.showIcon(IconNames.Asleep)
+    } else if (hp > 10 && hp <= 40) {
+        basic.showIcon(IconNames.Sad)
+    } else {
+        basic.showIcon(IconNames.Skull)
+    }
+})
